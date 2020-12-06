b16dedb5c08964c60fca941873fcb4ae37e07b56
Yampier Medina <jpmti2016@gmail.com>
2020-12-06 06:41:14 -0500

    Static generated pages without data

---
+++ pages/first-post.js
@@ -0,0 +1,47 @@
+import Link from "next/link";
+import Head from "next/head";
+import Layout from "../components/layout";
+export default function FirstPost() {
+  return (
+    <Layout className="container">
+      <Head>
+        <title>First Post</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <h1>First Post</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis
+        repellendus corrupti quos hic ipsa exercitationem?
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis
+        vitae ab quis, est consectetur iusto repellendus asperiores consequuntur
+        ducimus amet eos incidunt fugiat. Voluptatum quaerat alias culpa
+        necessitatibus ipsum quod adipisci veniam earum sit ipsa quo illo,
+        repellat quasi commodi aperiam nesciunt, nemo facilis? Esse perferendis
+        distinctio saepe cupiditate eligendi. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Odit distinctio ullam sequi aliquid
+        laborum repellat atque optio molestiae voluptates recusandae! Porro
+        architecto ipsum cum dolorum officia error, perferendis fuga, earum
+        quasi asperiores voluptas dolor? Cupiditate sunt vero atque suscipit a!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque
+        consequatur, qui, explicabo quod amet odio aliquid commodi ipsum dolorem
+        ratione, dolor impedit sint molestias in porro quas sit repellendus
+        quis. Eaque officia repellendus, praesentium qui quisquam necessitatibus
+        quas veniam suscipit eveniet accusamus! Aliquam corporis dolor iste quam
+        in doloribus libero velit assumenda dolore, adipisci nostrum beatae
+        illum nulla omnis numquam, id repellat ad, cumque minima reprehenderit
+        dolores obcaecati. Sequi odio accusantium consequuntur doloremque cum
+        illo aperiam cupiditate reprehenderit tenetur tempora.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur
+        suscipit cumque dignissimos dolore. Laborum sapiente officiis nihil
+        ratione, voluptatibus animi.
+      </p>
+    </Layout>
+  );
+}

87ce5fbf6376bd9563c9d70b2d9a080f5838f614
Yampier Medina <jpmti2016@gmail.com>
2020-12-07 00:49:05 -0500

    Render markdown and format post details

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Link from "next/link";
 import Layout, { siteTitle } from "../components/layout";
+import Date from "../components/date";
 import utilStyles from "../styles/utils.module.css";
 
 import { getSortedPostsData } from "../lib/posts";
@@ -38,11 +39,11 @@ export default function Home({ allPostsData }) {
           {console.log("posts: ", allPostsData)}
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>{date}</small>
             </li>
           ))}
         </ul>
